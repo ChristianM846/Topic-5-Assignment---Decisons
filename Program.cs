eaf7aaa44894841bafb25879285ee9773478d665
Christian Moyes <moyechri846@gmail.com>
2023-09-25 10:57:41 -0400

    In work

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@
             Console.WriteLine("1 - Simple Banking Machine");
             Console.WriteLine("2 - Parking Garage Cost");
             Console.WriteLine("3 - Hurricane");
-            Console.WriteLine("(Input the number corisponding to your selection)");
+            Console.WriteLine("(Input the number corresponding to your selection)");
 
             while (!Int32.TryParse(Console.ReadLine(), out selection) || selection > 3)
             {
-                Console.WriteLine("Good jon numbnut, it works");
+                Console.WriteLine("That is not a valid input. Please input the number that corresponds to your choice");
             }
 
             if (selection == 1)
@@ -29,14 +29,25 @@
             balance = 150;
             Console.WriteLine("Hello, welcome to the Bank Of Blorb. Please input the name of your desired transaction:");
             selection = Console.ReadLine().Trim().ToUpper();
-            Console.WriteLine(selection); //temporary
             if (selection == "DEPOSIT")
             {
                 Console.WriteLine("deposit");
             }
             else if (selection == "WITHDRAWAL")
             {
-                Console.WriteLine("withdrawal");
+                Console.WriteLine("How much would you like to withdrawal:");
+                if (Double.TryParse(Console.ReadLine(), out withdrawal))
+                {
+                    if (withdrawal <= balance - 0.75)
+                    {
+
+                    }
+                }
+                else
+                {
+                    balance = balance - 0.75;
+                    Console.WriteLine($"Invalid input, transaction cancelled, your account ballance is now {balance.ToString("C")}");
+                }
             }
             else if (selection == "BILL PAYMENT")
             {
@@ -46,32 +57,33 @@
                     if (payment <= balance - 0.75) 
                     {
                         balance = balance - payment - 0.75;
-                        Console.WriteLine($"Your payment has been made. You new account balance is {balance}. Have a nice day");
+                        Console.WriteLine($"Your payment has been made. You new account balance is {balance.ToString("C")}. Have a nice day");
                     }
                     else if (payment > balance - 0.75)
                     {
                         balance = balance - 0.75;
-                        Console.WriteLine($"You cannot pay more then you have. Transaction cancelled. Your account balance is now {balance}");
+                        Console.WriteLine($"You cannot pay more then you have. Transaction cancelled. Your account balance is now {balance.ToString("C")}");
                     }
                     else if (payment < 0)
                     {
                         balance = balance - 0.75;
-                        Console.WriteLine($"Thought you could cheat the system? NOPE! Transaction cancelled. Your account balance is now {balance}");
+                        Console.WriteLine($"Thought you could cheat the system? NOPE! Transaction cancelled. Your account balance is now {balance.ToString("C") }");
                     }
                 }
                 else
                 {
-                    Console.WriteLine($"Invalid input, transaction cancelled, your account ballance is now {balance}");
+                    balance = balance - 0.75;
+                    Console.WriteLine($"Invalid input, transaction cancelled, your account ballance is now {balance.ToString("C")}");
                 }
             }
             else if (selection == "ACCOUNT BALANCE UPDATE")
             {
                 balance = balance - 0.75;
-                Console.WriteLine($"Your current account balance is {balance}, have a nice day");
+                Console.WriteLine($"Your current account balance is {balance.ToString("C")}, have a nice day");
             }
             else
             {
-                Console.WriteLine($"Invalid input, transaction cancelled, your account ballance is now {balance}");
+                Console.WriteLine($"Invalid input, transaction cancelled, your account ballance is now {balance.ToString("C")}");
             }
 
         }
